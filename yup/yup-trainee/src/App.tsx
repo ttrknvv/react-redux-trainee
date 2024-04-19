22dd3649e6554bb968c0830cced732a12d945d78
ttrknvv <tarakanov@completesoft.net>
2024-04-19 16:09:31 +0300

    final learn yup with form

--- yup/yup-trainee/src/App.tsx
+++ yup/yup-trainee/src/App.tsx
@@ -1,29 +1,37 @@
 import React from 'react';
 import * as yup from "yup";
 import './App.css';
-import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import { Controller, SubmitErrorHandler, SubmitHandler, useForm } from 'react-hook-form';
+import { yupResolver } from "@hookform/resolvers/yup";
 
 interface formTestYUP {
   name : string;
   age: number;
-  date: string;
+  date: Date;
+  email: string;
 }
 
-const fromTestShema = yup.object({
-  name: yup.string(),
-  age: yup.number().required(),
-  date: yup.date()
-})
+const formTestShema = yup.object().shape({
+  name: yup.string().required().uppercase(),
+  age: yup.number().required("Required error age").max(20).min(8).when('name', {
+    is: 'nikita',
+    then: (schema) => schema.min(10),
+}),
+  date: yup.date().required("DATE REQUIRED"),
+  email: yup.string().email("EMAIL ERROR").required("required email")
+});
 
 function App() {
 
-const {control, handleSubmit, formState: { errors }} = useForm<formTestYUP>();
+const {control, handleSubmit, formState: { errors }} = useForm<formTestYUP>({ resolver: yupResolver(formTestShema) });
 
-const onSubmit: SubmitHandler<formTestYUP> = (data) => {console.log(data); console.log(errors)}
+const onSubmit: SubmitHandler<formTestYUP> = (data) => {console.log(data); };
+const onError: SubmitErrorHandler<formTestYUP> = (data) => {console.log(errors); }
+
 
   return (
     <div className="App">
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmit, onError)}>
         <label>Name:</label>
         <Controller name="name"
         control={control}
@@ -36,8 +44,14 @@ const onSubmit: SubmitHandler<formTestYUP> = (data) => {console.log(data); conso
         <label>Date:</label>
         <Controller name="date"
         control={control}
+        render={({field}) => <input {...field} type='date' value={String(field.value)}/>} />
+        <label>Email:</label>
+        <Controller name="email"
+        control={control}
         render={({field}) => <input {...field}/>} />
-        <button disabled={!!errors.age || !!errors.date || !!errors.name}>SUBMIT</button>
+        
+        {/* <button disabled={!!errors.age || !!errors.date || !!errors.name}>SUBMIT</button> */}
+        <button>SUBMIT</button>
       </form>
     </div>
   );
